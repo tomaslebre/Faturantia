032690a6508332b56b07b2044c1a78814d7838d5
Sebastião Rusu <20220905@iade.pt>
2023-11-16 21:36:00 +0000

    Página fatura view semi-concluida

--- app/src/main/java/pt/iade/sebastiaorusu/myapplication/FaturaActivity.java
+++ app/src/main/java/pt/iade/sebastiaorusu/myapplication/FaturaActivity.java
@@ -1,8 +1,11 @@
 package pt.iade.sebastiaorusu.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.content.Context;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,18 +24,32 @@ public class FaturaActivity extends AppCompatActivity {
 
     private void setupComponents() {
         purchaseDateEdit = findViewById(R.id.purchase_date_edit);
+        purchaseDateCalendar = findViewById(R.id.purchase_date_calendar);
+
         purchaseDateEdit.setOnFocusChangeListener((v, hasFocus) -> {
             if (hasFocus) {
-                purchaseDateCalendar.setVisibility(CalendarView.VISIBLE);
+                // Quando o EditText recebe foco, esconde o teclado e mostra o CalendarView
+                hideKeyboard();
+                purchaseDateCalendar.setVisibility(View.VISIBLE);
+
             } else {
-                purchaseDateCalendar.setVisibility(CalendarView.GONE);
+                purchaseDateCalendar.setVisibility(View.GONE);
             }
         });
 
-        purchaseDateCalendar = findViewById(R.id.purchase_date_calendar);
         purchaseDateCalendar.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            // Quando a data é alterada no CalendarView, atualiza o EditText
             String date = dayOfMonth + "/" + (month + 1) + "/" + year;
             purchaseDateEdit.setText(date);
         });
     }
+
+    public void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        View currentFocus = getCurrentFocus();
+        if (currentFocus != null) {
+            imm.hideSoftInputFromWindow(currentFocus.getWindowToken(), 0);
+        }
+    }
+
 }
