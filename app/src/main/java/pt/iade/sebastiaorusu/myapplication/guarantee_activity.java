ad76f17e688434af07a1632f54eac73c0560a688
Sebastião Rusu <20220905@iade.pt>
2023-11-19 02:31:36 +0000

    finalização da páginal bill e inico da pagina guarantee

---
+++ app/src/main/java/pt/iade/sebastiaorusu/myapplication/guarantee_activity.java
@@ -0,0 +1,14 @@
+package pt.iade.sebastiaorusu.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class guarantee_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guarantee);
+    }
+}
