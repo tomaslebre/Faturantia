37e256a3b1d23e980c124a8b005d30c47465aa77
Sebastião Rusu <20220905@iade.pt>
2023-11-24 13:06:39 +0000

    remodelação do calendário

--- app/src/main/java/pt/iade/sebastiaorusu/myapplication/FaturaActivity.java
+++ app/src/main/java/pt/iade/sebastiaorusu/myapplication/FaturaActivity.java
@@ -2,6 +2,7 @@ package pt.iade.sebastiaorusu.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -23,6 +24,7 @@ public class FaturaActivity extends AppCompatActivity {
         setContentView(R.layout.fat_view);
         setupComponents();
     }
+
     private void setupComponents() {
         purchaseDateEdit = findViewById(R.id.purchase_date_edit);
         purchaseDateCalendar = findViewById(R.id.purchase_date_calendar);
@@ -35,6 +37,18 @@ public class FaturaActivity extends AppCompatActivity {
                 purchaseDateCalendar.setVisibility(View.GONE);
             }
         });
+        purchaseDateEdit.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.onTouchEvent(event);
+                InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null && purchaseDateEdit.hasSelection()) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+                return true;
+            }
+        });
+
         purchaseDateCalendar.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
             // Quando a data é alterada no CalendarView, atualiza o EditText
             String date = dayOfMonth + "/" + (month + 1) + "/" + year;
