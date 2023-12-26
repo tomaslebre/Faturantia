05e267469a0c54190e0351ec613a91db685ba8ee
Sebastião Rusu <20220905@iade.pt>
2023-12-26 23:57:01 +0000

    inicio da garantias importantes

---
+++ app/src/main/java/pt/iade/sebastiaorusu/myapplication/ImptGuarantee.java
@@ -0,0 +1,14 @@
+package pt.iade.sebastiaorusu.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImptGuarantee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_impt_guarantee);
+    }
+}
