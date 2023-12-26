6a255c4a8c7e1b2ceaf23323e97a036e7e77da1c
tomaslebre <20220907@iade.pt>
2023-12-26 02:16:15 +0000

    pagina inf pessoal

---
+++ app/src/main/java/pt/iade/sebastiaorusu/myapplication/InfPersonalActivity.java
@@ -0,0 +1,14 @@
+package pt.iade.sebastiaorusu.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfPersonalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inf_personal);
+    }
+}
