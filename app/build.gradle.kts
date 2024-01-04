ab9e049f5c6cac468e4ba151bacc1b4acb5f4edf
Sebastião Rusu <20220905@iade.pt>
2024-01-04 01:54:58 +0000

    Conexão android studio com spring-boot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,12 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // https://mvnrepository.com/artifact/com.squareup.retrofit2/retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // https://mvnrepository.com/artifact/com.squareup.retrofit2/converter-gson
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
+
 }
