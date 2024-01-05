55b43d691060845d5227d28dce9eeb96c38589b5
tomaslebre <20220907@iade.pt>
2024-01-05 21:07:58 +0000

    user location

--- app/src/main/java/pt/iade/sebastiaorusu/myapplication/models/UserItem.java
+++ app/src/main/java/pt/iade/sebastiaorusu/myapplication/models/UserItem.java
@@ -8,14 +8,19 @@ public class UserItem implements Serializable {
     private String email;
     private String password;
 
+    private String name;
+
+    private String location;
+
     public UserItem() {
-        this(0, "", "");
+        this(0, "", "", "", "");
     }
 
-    public UserItem(int id, String email, String password) {
+    public UserItem(int id, String email, String password, String name, String location) {
         this.id = id;
         this.email = email;
         this.password = password;
+        this.name = name;
     }
 
     public void save() {
@@ -49,5 +54,21 @@ public class UserItem implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 }
