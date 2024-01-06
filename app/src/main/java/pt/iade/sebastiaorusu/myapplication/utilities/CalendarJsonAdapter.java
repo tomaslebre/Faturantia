4ee6189fcf4164c0b08de55e653087f9d1472693
tomaslebre <20220907@iade.pt>
2024-01-06 16:39:19 +0000

    nova tentativa 2

--- app/src/main/java/pt/iade/sebastiaorusu/myapplication/utilities/CalendarJsonAdapter.java
+++ app/src/main/java/pt/iade/sebastiaorusu/myapplication/utilities/CalendarJsonAdapter.java
@@ -1,8 +1,8 @@
 package pt.iade.sebastiaorusu.myapplication.utilities;
-import com.google.gson.Gson;
+
+
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
@@ -11,7 +11,6 @@ import com.google.gson.JsonDeserializer;
 
 import java.lang.reflect.Type;
 import java.time.LocalDate;
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -19,13 +18,13 @@ import java.util.Locale;
 public class CalendarJsonAdapter implements JsonSerializer<Calendar>, JsonDeserializer<Calendar> {
     @Override
     public JsonElement serialize(Calendar src, Type typeOfSrc, JsonSerializationContext context) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.ENGLISH);
         return new JsonPrimitive(formatter.format(((GregorianCalendar)src).toZonedDateTime()));
     }
 
     @Override
     public Calendar deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.ENGLISH);
         LocalDate localDate = LocalDate.parse(json.getAsString(), formatter);
 
         return new GregorianCalendar(localDate.getYear(), localDate.getMonthValue(), localDate.getDayOfMonth());
